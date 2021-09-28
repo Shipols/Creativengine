45b2e8941726c8fa66f85c5d3a737740358f7834
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-09-28 18:55:31 +0300

    Added shaders support (Basic.shader) GLSL File.

--- Creativengine/src/Application.cpp
+++ Creativengine/src/Application.cpp
@@ -1,9 +1,44 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
 #include "Logger/LoggerMacros.h"
 
+#include "LibraryIncludes.h"
+
 namespace Creativengine {
+
+	struct ShaderProgramSource
+	{
+		std::string VertexSource;
+		std::string FragmentSource;
+	};
+
+	static ShaderProgramSource ParseShader(const std::string& filepath)
+	{
+		std::ifstream stream(filepath);
+
+		enum class ShaderType
+		{
+			NONE = -1, VERTEX = 0, FRAGMENT = 1
+		};
+
+		std::string line;
+		std::stringstream ss[2];
+		ShaderType type = ShaderType::NONE;
+		while (getline(stream, line))
+		{
+			if (line.find("#shader") != std::string::npos)
+			{
+				if (line.find("vertex") != std::string::npos)
+					type = ShaderType::VERTEX;
+				else if (line.find("fragment") != std::string::npos)
+					type = ShaderType::FRAGMENT;
+			}
+			else
+			{
+				ss[(int)type] << line << '\n';
+			}
+		}
+
+		return { ss[0].str(), ss[1].str() };
+	}
 
 	static unsigned int CompileShader(unsigned int type, const std::string& source)
 	{
@@ -89,27 +124,8 @@ namespace Creativengine {
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		std::string vertexShader =
-			"#version 330 core\n"
-			"\n"
-			"layout(location = 0) in vec4 position;\n"
-			"\n"
-			"void main()\n"
-			"{\n"
-			"	gl_Position = position;\n"
-			"}\n";
-
-		std::string fragmentShader =
-			"#version 330 core\n"
-			"\n"
-			"layout(location = 0) out vec4 color;\n"
-			"\n"
-			"void main()\n"
-			"{\n"
-			"	color = vec4(0.8, 0.9, 0.4, 1.0);\n"
-			"}\n";
-
-		unsigned int shader = CreateShader(vertexShader, fragmentShader);
+		ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 		glUseProgram(shader);
 
 		/* Loop until the user closes the window */
---
+++ Creativengine/src/LibraryIncludes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
