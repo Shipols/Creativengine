241792b02e5d87aa30772cab1d25332919aea59d
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-10-02 12:50:29 +0300

    little changes

--- Creativengine/src/ImGui/ImGuiDockspace.cpp
+++ Creativengine/src/ImGui/ImGuiDockspace.cpp
@@ -32,7 +32,7 @@ namespace Creativengine {
 
 		if (!opt_padding)
 			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("DockSpace Demo", (bool*)true, window_flags);
+		ImGui::Begin("DockSpace", (bool*)true, window_flags);
 		if (!opt_padding)
 			ImGui::PopStyleVar();
 
