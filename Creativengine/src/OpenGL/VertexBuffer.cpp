3c43022ec1cb13ab98c40b57067a2b6934ab2325
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-10-07 20:13:24 +0300

    Add files via upload

--- Creativengine/src/OpenGL/VertexBuffer.cpp
+++ Creativengine/src/OpenGL/VertexBuffer.cpp
@@ -2,28 +2,24 @@
 
 #include "Renderer.h"
 
-namespace Creativengine {
+VertexBuffer::VertexBuffer(const void* data, unsigned int size)
+{
+	GLCall(glGenBuffers(1, &m_RendererID));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
 
-	VertexBuffer::VertexBuffer(const void* data, unsigned int size)
-	{
-		GLCall(glGenBuffers(1, &m_RenderID));
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RenderID));
-		GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
-	}
+VertexBuffer::~VertexBuffer()
+{
+	GLCall(glDeleteBuffers(1, &m_RendererID));
+}
 
-	VertexBuffer::~VertexBuffer()
-	{
-		GLCall(glDeleteBuffers(1, &m_RenderID));
-	}
+void VertexBuffer::Bind() const
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+}
 
-	void VertexBuffer::Bind() const
-	{
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RenderID));
-	}
-
-	void VertexBuffer::Unbind() const
-	{
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	}
-
+void VertexBuffer::Unbind() const
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 }
--- Creativengine/src/OpenGL/VertexBuffer.h
+++ Creativengine/src/OpenGL/VertexBuffer.h
@@ -1,20 +1,13 @@
 #pragma once
 
-namespace Creativengine {
+class VertexBuffer
+{
+private:
+	unsigned int m_RendererID;
+public:
+	VertexBuffer(const void* data, unsigned int size);
+	~VertexBuffer();
 
-	class VertexBuffer
-	{
-	private:
-
-		unsigned int m_RenderID;
-
-	public:
-
-		VertexBuffer(const void* data, unsigned int size);
-		~VertexBuffer();
-
-		void Bind() const;
-		void Unbind() const;
-	};
-
-}
+	void Bind() const;
+	void Unbind() const;
+};
