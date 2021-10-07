fd4cf69708390b48d55ed4781133a769cbe97ad7
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-10-07 21:47:57 +0300

    Revert "Buffer Layout Abstraction."
    This reverts commit b7b311eff19accbd24f4f7c28764f3bc4cfe1285.

--- Creativengine/src/OpenGL/VertexArray.cpp
+++ Creativengine/src/OpenGL/VertexArray.cpp
@@ -3,42 +3,38 @@
 #include "VertexBufferLayout.h"
 #include "Renderer.h"
 
-namespace Creativengine {
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_RendererID));
+}
 
-	VertexArray::VertexArray()
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer & vb, const VertexBufferLayout & layout)
+{
+	Bind();
+	vb.Bind();
+	const auto& elements = layout.GetElements();
+	unsigned int offset = 0;
+	for (unsigned int i = 0;i < elements.size(); i++)
 	{
-		GLCall(glGenVertexArrays(1, &m_RendererID));
+		const auto& element = elements[i];
+		GLCall(glEnableVertexAttribArray(0));
+		GLCall(glVertexAttribPointer(i, element.count, element.type,
+			element.normalized, layout.GetStride(), (const void*)offset));
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
 	}
+}
 
-	VertexArray::~VertexArray()
-	{
-		GLCall(glDeleteVertexArrays(1, &m_RendererID));
-	}
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_RendererID));
+}
 
-	void VertexArray::AddBuffer(const VertexBuffer & vb, const VertexBufferLayout & layout)
-	{
-		Bind();
-		vb.Bind();
-		const auto& elements = layout.GetElements();
-		unsigned int offset = 0;
-		for (unsigned int i = 0; i < elements.size(); i++)
-		{
-			const auto& element = elements[i];
-			GLCall(glEnableVertexAttribArray(0));
-			GLCall(glVertexAttribPointer(i, element.count, element.type,
-				element.normalized, layout.GetStride(), (const void*)offset));
-			offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
-		}
-	}
-
-	void VertexArray::Bind() const
-	{
-		GLCall(glBindVertexArray(m_RendererID));
-	}
-
-	void VertexArray::Unbind() const
-	{
-		GLCall(glBindVertexArray(0));
-	}
-
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
 }
--- Creativengine/src/OpenGL/VertexBuffer.cpp
+++ Creativengine/src/OpenGL/VertexBuffer.cpp
@@ -2,28 +2,24 @@
 
 #include "Renderer.h"
 
-namespace Creativengine {
+VertexBuffer::VertexBuffer(const void* data, unsigned int size)
+{
+	GLCall(glGenBuffers(1, &m_RendererID));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
 
-	VertexBuffer::VertexBuffer(const void* data, unsigned int size)
-	{
-		GLCall(glGenBuffers(1, &m_RendererID));
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
-		GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
-	}
+VertexBuffer::~VertexBuffer()
+{
+	GLCall(glDeleteBuffers(1, &m_RendererID));
+}
 
-	VertexBuffer::~VertexBuffer()
-	{
-		GLCall(glDeleteBuffers(1, &m_RendererID));
-	}
+void VertexBuffer::Bind() const
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+}
 
-	void VertexBuffer::Bind() const
-	{
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
-	}
-
-	void VertexBuffer::Unbind() const
-	{
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	}
-
+void VertexBuffer::Unbind() const
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 }
