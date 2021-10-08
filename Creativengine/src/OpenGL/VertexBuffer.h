41621162a2cf4c716e9813950cab171a4ba76cd1
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-10-08 18:54:30 +0300

    Revert "Revert "Buffer Layout Abstraction.""
    This reverts commit fd4cf69708390b48d55ed4781133a769cbe97ad7.

--- Creativengine/src/OpenGL/VertexBuffer.h
+++ Creativengine/src/OpenGL/VertexBuffer.h
@@ -1,13 +1,17 @@
 #pragma once
 
-class VertexBuffer
-{
-private:
-	unsigned int m_RendererID;
-public:
-	VertexBuffer(const void* data, unsigned int size);
-	~VertexBuffer();
+namespace Creativengine {
 
-	void Bind() const;
-	void Unbind() const;
-};
+	class VertexBuffer
+	{
+	private:
+		unsigned int m_RendererID;
+	public:
+		VertexBuffer(const void* data, unsigned int size);
+		~VertexBuffer();
+
+		void Bind() const;
+		void Unbind() const;
+	};
+
+}
