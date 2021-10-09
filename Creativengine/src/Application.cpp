7862b95d3d5e6c80e6814bb99091bdbd78268dc4
Retr0A <71063716+Ludiliste@users.noreply.github.com>
2021-10-09 12:12:40 +0300

    little changes and fixes

--- Creativengine/src/Application.cpp
+++ Creativengine/src/Application.cpp
@@ -20,79 +20,6 @@
 
 namespace Creativengine {
 
-	/*struct ShaderProgramSource
-	{
-		std::string VertexSource;
-		std::string FragmentSource;
-	};
-
-	static ShaderProgramSource ParseShader(const std::string& filepath)
-	{
-		std::ifstream stream(filepath);
-
-		enum class ShaderType
-		{
-			NONE = -1, VERTEX = 0, FRAGMENT = 1
-		};
-
-		std::string line;
-		std::stringstream ss[2];
-		ShaderType type = ShaderType::NONE;
-		while (getline(stream, line))
-		{
-			if (line.find("#shader") != std::string::npos)
-			{
-				if (line.find("vertex") != std::string::npos)
-					type = ShaderType::VERTEX;
-				else if (line.find("fragment") != std::string::npos)
-					type = ShaderType::FRAGMENT;
-			}
-			else
-			{
-				ss[(int)type] << line << '\n';
-			}
-		}
-
-		return { ss[0].str(), ss[1].str() };
-	}
-
-	static unsigned int CompileShader(unsigned int type, const std::string& source)
-	{
-		unsigned int id = glCreateShader(type);
-		const char* src = source.c_str();
-		glShaderSource(id, 1, &src, nullptr);
-		glCompileShader(id);
-
-		int result;
-		glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-		if (result == GL_FALSE)
-		{
-			int length;
-			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-			char* message = (char*)alloca(length * sizeof(char));
-			glGetShaderInfoLog(id, length, &length, message);
-			PrintError("Failed to compile shader!");
-			glDeleteShader(id);
-			return 0;
-		}
-
-		return id;
-	}
-
-	static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-	{
-		GLCall(unsigned int program = glCreateProgram());
-		unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-		unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-		glAttachShader(program, vs);
-		glAttachShader(program, fs);
-		glLinkProgram(program);
-		glValidateProgram(program);
-
-		return program;
-	}*/
-
 	__declspec(dllexport) int Run()
 	{
 
@@ -179,6 +106,8 @@ namespace Creativengine {
 
 		#pragma endregion
 
+		glViewport(120, 200, 1080 - 200, 600 - 200);
+		
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
@@ -222,9 +151,10 @@ namespace Creativengine {
 			/* Poll for and process events */
 			GLCall(glfwPollEvents());
 
+
 			glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
 
-				glViewport(0, 0, width, height);
+				glViewport(120, 200, width - 200, height - 200);
 
 			});
 		}
